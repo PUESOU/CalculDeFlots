5f4630aa533f4027f2b20f682e72c7d9a0142450
Adnyropast <dealamu134579@gmail.com>
2020-03-13 22:31:09 +0100

    Created Test class to show off the ReseauTransport class and getBranches/getPaths methods

---
+++ src/Test.java
@@ -0,0 +1,47 @@
+
+import java.util.List;
+import java.util.Iterator;
+
+public class Test {
+	public static void main(String[] args) {
+		ReseauTransport reseauTransport = new ReseauTransport(5, 5);// {E, 1, 2, 3, 4, 5, a, b, c, d, e, S}
+		
+		Noeud.stringType = 1;// Affichage des nœuds : "id(suivants…)".
+		
+		System.out.println("\n---- Noeud(suivants…) ----");
+		System.out.println(reseauTransport);
+		
+		Noeud.stringType = 0;// Affichage des nœuds : "id".
+		
+		System.out.println("\n---- Branches ----");
+		System.out.println(listJoin(reseauTransport.getNoeud("E").getBranches(), "\n"));
+		
+		System.out.println("\n---- Chemins ----");
+		System.out.println(listJoin(reseauTransport.getNoeud("E").getPaths(), "\n"));
+		
+		Noeud.stringType = 1;// Remet l'affichage par défaut.
+	}
+	
+	/**
+	 * Retourne une chaîne de caractères construite à partir d'une liste, les éléments étant séparés par un séparateur donné.
+	 * @param <T>
+	 * @param list la liste à partir de laquelle créer la chaîne.
+	 * @param separator le séparateur devant séparer les éléments.
+	 * @return la chaîne de caractères représentant la liste.
+	 */
+	
+	public static <T> String listJoin(List<T> list, String separator) {
+		StringBuffer buf = new StringBuffer();
+		
+		for(Iterator<T> it = list.iterator(); it.hasNext(); ) {
+			T t = it.next();
+			buf.append(t);
+			
+			if(it.hasNext()) {
+				buf.append(separator);
+			}
+		}
+		
+		return buf.toString();
+	}
+}
