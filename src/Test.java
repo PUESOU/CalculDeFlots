4490bb372e2dc107dbd27f473553a51715abae87
PUESOU <PUESOU.TRMXXYZ@gmail.com>
2020-03-19 18:28:19 +0100

    Removed example0 and example1 methods, just import coursFF.csv and coursBG.csv instead of using those

--- src/Test.java
+++ src/Test.java
@@ -32,56 +32,6 @@ public class Test {
 		return buf.toString();
 	}
 	
-	public static ReseauTransport example0() {
-		ReseauTransport reseauTransport = new ReseauTransport();
-		
-		reseauTransport.addNoeud("E");
-		
-		for(int i = 0; i < 3; ++i) {
-			reseauTransport.addNoeud(i+1);
-		}
-		
-		for(int i = 0; i < 4; ++i) {
-			reseauTransport.addNoeud((char)('a' + i) + "");
-		}
-		
-		reseauTransport.addNoeud("S");
-		
-		reseauTransport.addArc("E", "1");
-		reseauTransport.addArc("E", "2");
-		reseauTransport.addArc("E", "3");
-		reseauTransport.addArc("1", "a");
-		reseauTransport.addArc("1", "b");
-		reseauTransport.addArc("1", "d");
-		reseauTransport.addArc("2", "a");
-		reseauTransport.addArc("2", "b");
-		reseauTransport.addArc("2", "c");
-		reseauTransport.addArc("3", "c");
-		reseauTransport.addArc("3", "d");
-		reseauTransport.addArc("a", "S");
-		reseauTransport.addArc("b", "S");
-		reseauTransport.addArc("c", "S");
-		reseauTransport.addArc("d", "S");
-		
-		reseauTransport.findArc("E", "1").setCapacite(45);
-		reseauTransport.findArc("E", "2").setCapacite(25);
-		reseauTransport.findArc("E", "3").setCapacite(25);
-		reseauTransport.findArc("1", "a").setCapacite(10);
-		reseauTransport.findArc("1", "b").setCapacite(15);
-		reseauTransport.findArc("1", "d").setCapacite(20);
-		reseauTransport.findArc("2", "a").setCapacite(20);
-		reseauTransport.findArc("2", "b").setCapacite(5);
-		reseauTransport.findArc("2", "c").setCapacite(5);
-		reseauTransport.findArc("3", "c").setCapacite(10);
-		reseauTransport.findArc("3", "d").setCapacite(10);
-		reseauTransport.findArc("a", "S").setCapacite(30);
-		reseauTransport.findArc("b", "S").setCapacite(10);
-		reseauTransport.findArc("c", "S").setCapacite(20);
-		reseauTransport.findArc("d", "S").setCapacite(30);
-		
-		return reseauTransport;
-	}
-	
 	public static void print(ReseauTransport reseauTransport) {
 		Noeud.stringType = 1;// Affichage des nœuds : "id(suivants…)".
 		
@@ -97,34 +47,6 @@ public class Test {
 		System.out.println(listJoin(reseauTransport.getNoeud("E").getPaths(), "\n"));
 		
 		Noeud.stringType = 1;// Remet l'affichage par défaut.
-	}
-	
-	public static ReseauTransport example1() {
-		ReseauTransport reseauTransport = new ReseauTransport();
-		
-		reseauTransport.addNoeud("E");
-		reseauTransport.addNoeud("1");
-		reseauTransport.addNoeud("2");
-		reseauTransport.addNoeud("3");
-		reseauTransport.addNoeud("S");
-		
-		reseauTransport.addArc("E", "1");
-		reseauTransport.addArc("E", "2");
-		reseauTransport.addArc("1", "2");
-		reseauTransport.addArc("1", "3");
-		reseauTransport.addArc("2", "3");
-		reseauTransport.addArc("2", "S");
-		reseauTransport.addArc("3", "S");
-		
-		reseauTransport.findArc("E", "1").setCapacite(30).setPoids(7);
-		reseauTransport.findArc("E", "2").setCapacite(20).setPoids(6);
-		reseauTransport.findArc("1", "2").setCapacite(25).setPoids(5);
-		reseauTransport.findArc("1", "3").setCapacite(10).setPoids(4);
-		reseauTransport.findArc("2", "3").setCapacite(20).setPoids(2);
-		reseauTransport.findArc("2", "S").setCapacite(25).setPoids(2);
-		reseauTransport.findArc("3", "S").setCapacite(20).setPoids(1);
-		
-		return reseauTransport;
 	}
 	
 }
