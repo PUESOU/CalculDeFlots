de5ca450807b8f94ab8e7a5cf0f755887a1f1cba
PUESOU <PUESOU.TRMXXYZ@gmail.com>
2020-03-19 22:46:54 +0100

    Fixed file error problems in OtherTest

--- src/OtherTest.java
+++ src/OtherTest.java
@@ -1,26 +1,29 @@
 
 public class OtherTest {
-
-	public void createRecords(String fileName, int count) {
-		/*** {E, 1, 2, 3, 4, 5, a, b, c, d, e, S} */
-		ReseauTransport reseauTransport1 = new ReseauTransport(5, 5);
-		ReseauTransport reseauTransport2 = new ReseauTransport(reseauTransport1);
-
-		MaxFlowSolver algo1 = new FullFordFulkerson();
-		MaxFlowSolver algo2 = new BusackerGowen();
-
-		Thread thread1 = new Thread(new ExecTimeTest(count, algo1, fileName + "-FullFord.csv", reseauTransport1));
-		Thread thread2 = new Thread(new ExecTimeTest(count, algo2, fileName + "-Busacker.csv", reseauTransport2));
-
-		thread1.start();
-		thread2.start();
-
-	}
-
-	public static void main(String[] args) {
-		int count = 100;
-		OtherTest test = new OtherTest();
-		test.createRecords("Comparaison" + count, count);
-	}
 	
+	public static final int COUNT = 100;
+	
+    public static final String CSVFILENAME = "coursFF";
+    
+    public void createRecords(String fileName, int count) {
+        /*** {E, 1, 2, 3, 4, 5, a, b, c, d, e, S} */
+        ReseauTransport reseauTransport1 = new ReseauTransport(fileName + ".csv");
+        ReseauTransport reseauTransport2 = new ReseauTransport(fileName + ".csv");
+        
+        MaxFlowSolver algo1 = new FullFordFulkerson();
+        MaxFlowSolver algo2 = new BusackerGowen();
+        
+        Thread thread1 = new Thread(new ExecTimeTest(count, algo1, fileName + "-FullFord.csv", reseauTransport1));
+        Thread thread2 = new Thread(new ExecTimeTest(count, algo2, fileName + "-Busacker.csv", reseauTransport2));
+        
+        thread1.start();
+        thread2.start();
+        
+    }
+    
+    public static void main(String[] args) {
+        OtherTest test = new OtherTest();
+        test.createRecords(OtherTest.CSVFILENAME, OtherTest.COUNT);
+    }
+    
 }
